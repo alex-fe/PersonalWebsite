dbf13290a1c276789cc17ef2950054202692dcbf
Alex Feldman <alexfeldman93@gmail.com>
2018-03-23 13:31:08 -0700

    Add blog urls, templates

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import path
+
+from .views import PostListView
+
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='posts')
+]
