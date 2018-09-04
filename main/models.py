23dd217933ef4a6c2c0f31429c082b2e8787ace9
Alex Feldman <alexfeldman93@gmail.com>
2018-09-04 01:13:57 -0700

    imImage table set. Need to fix links

--- main/models.py
+++ main/models.py
@@ -1,6 +1,10 @@
+import os
+
 from django.db import models
 
-# cooking, coding, soldering, music, About, rand.
+
+def get_image_path(instance, filename):
+    return os.path.join('splash', instance.photo_dir, filename)
 
 
 class Catagory(models.Model):
@@ -9,6 +13,11 @@ class Catagory(models.Model):
     """
     name = models.CharField(max_length=50)
     detail = models.TextField(blank=True)
+    image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def photo_dir(self):
+        return '{}_{}'.format(self.id, self.name)
--- personal_site/urls.py
+++ personal_site/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
@@ -12,4 +14,4 @@ urlpatterns = [
         include(('aboutme.urls', 'aboutme'), namespace='about_me')
     ),
     path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
