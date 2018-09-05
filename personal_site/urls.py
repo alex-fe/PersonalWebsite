e8175c9b6ac49efdddd14151c7f6095791e43fe9
Alex Feldman <alexfeldman93@gmail.com>
2018-09-04 18:04:17 -0700

    Update static urls

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls import include
 from django.urls import path
 
@@ -12,4 +13,6 @@ urlpatterns = [
     path('', splash_page,  name='splash'),
     path('<slug:catagory>/', include(('blog.urls', 'blog'), namespace='blog')),
     path('<slug:tag>/', TagListView.as_view(), name='tags'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
