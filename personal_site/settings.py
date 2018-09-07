19b1548b9725264fe19947459292c1d626c2e2af
Alex Feldman <alexfeldman93@gmail.com>
2018-09-07 13:54:10 -0700

    update settings

--- personal_site/settings.py
+++ personal_site/settings.py
@@ -9,10 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
 import os
-
-from .personal_info import secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = os.environ['ALEX_PERSONAL_SITE_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
